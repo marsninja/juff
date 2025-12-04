79830aac07b4eefcbee9bc6ac74975e266f6077b
marsninja <jason@mars.ninja>
2025-12-04 09:20:48 -0500

    Vibe coding sucks, trying new strategy of removing all tests and going through codebase with fine tooth comb

--- tests/fixtures/configs/integration_project/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py with F401 violations
-# Per-file-ignores should suppress F401 here
-import os
-import sys
-import json
--- tests/fixtures/has_unused_imports.py
+++
@@ -1,8 +0,0 @@
-# File with unused imports (F401)
-import os
-import sys
-import json
-import re
-
-def hello():
-    return "hello"
